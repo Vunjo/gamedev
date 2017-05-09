bd882e56c66939f99d1ba1db8fb4e22ea937c7bd
Vunjo <VutosORPG@gmail.com>
2017-05-10 00:41:22 +0200

    optimize rotation matrix

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ int main (){
     printf("y = ");
     scanf("%f", &y);
     m[0] = cos((M_PI/180) * a);
-    m[1] = sin((M_PI/180) * a * (-1));
-    m[2] = sin((M_PI/180) * a);
-    m[3] = cos((M_PI/180) * a);
+    m[1] = -sin((M_PI/180) * a);
+    m[2] = -m[1];
+    m[3] = m[0];
 
     printf("%.2f,%.2f\n%.2f,%.2f\n", m[0], m[1], m[2], m[3]);
 
